45b5e13c6b909a1f99e2f7a93c0826e3a8433d88
Caio Valenca <cjvguimaraes@hotmail.com>
2018-10-07 16:02:13 -0300

    fixes to home.php

--- home.php
+++ home.php
@@ -8,25 +8,26 @@
     $arquivo_viagens = "arquivos/viagens.txt";
     $arquivo_viagens_dos_sonhos = "arquivos/viagens_dos_sonhos.txt";
     $conteudo = file_get_contents($arquivo_viagens);
-    $conteudo = $conteudo . "\r\n" . file_get_contents($arquivo_viagens_dos_sonhos);
+    if(filesize($arquivo_viagens_dos_sonhos) !=0)
+        $conteudo = $conteudo . "\r\n" . file_get_contents($arquivo_viagens_dos_sonhos);
     // Separa as linhas
     $linhas = explode ("\r\n", $conteudo);
+    
     // Separa os registros
     for ($i = 0; $i < sizeof($linhas); $i++) {
         list($destinos[$i], $datas[$i],$diarias[$i], $transportes[$i], $translados[$i], $estrelas[$i], $passeios[$i], $user[$i])= explode ("|", $linhas [$i]);
     }
 
-    echo $conteudo;
     $arquivo_usuario = "arquivos/usuarios.txt";
     $conteudo = file_get_contents($arquivo_usuario);
     // Separa as linhas
-    $linhas = explode ("\r\n", $conteudo);
+    $linhas_usuario = explode ("\r\n", $conteudo);
 
     // Separa os registros
-    for ($i = 0; $i < sizeof($linhas); $i++) {
-        list($nome[$i], $sobrenome[$i], $data[$i], $endereco[$i], $telefone[$i], $sexo[$i], $rg[$i], $cpf[$i], $foto[$i], $usuario[$i], $senha[$i]) = explode ("|", $linhas [$i]);
+    for ($i = 0; $i < sizeof($linhas_usuario); $i++) {
+        list($nome[$i], $sobrenome[$i], $data[$i], $endereco[$i], $telefone[$i], $sexo[$i], $rg[$i], $cpf[$i], $foto[$i], $usuario[$i], $senha[$i]) = explode ("|", $linhas_usuario [$i]);
     }
-    for ($i = 0; $i < sizeof($linhas); $i++) {
+    for ($i = 0; $i < sizeof($linhas_usuario); $i++) {
         if($sexo[$i]=='m'){
             $sexo[$i] = "Masculino";
         } else if($sexo[$i]=='f'){
@@ -57,98 +58,88 @@
 
         <div>
         <table align='center' border='0' width =100%>
-        <tr>
-            <td width="30%">
-                <table align='center' border='0' width =100%>
-                    <tr  align='center'>   
-                        <td colspan='10' ><h1>Usuário</h1></td>
-                    </tr>
-                    
-            
-                <?php
-                    for($i=0;$i<sizeof($linhas);$i++){
-                        if($usuario[$i]==$_SESSION['usuario_sessao']){
-                            echo "
-                                <tr>   
-                                    <td>Nome</td>    
-                                    <td>$nome[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td>Sobrenome</td>    
-                                    <td>$sobrenome[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td>Data de nascimento</td>    
-                                    <td>$data[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td>Enderço</td>    
-                                    <td>$endereco[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td>Telefone</td>    
-                                    <td>$telefone[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td>Sexo</td>    
-                                    <td>$sexo[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td>RG</td>    
-                                    <td>$rg[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td>CPF</td>    
-                                    <td>$cpf[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td>Login</td>    
-                                    <td>$usuario[$i]</td>
-                                <tr>
-                                <tr>   
-                                    <td colspan='2'><img width ='80%' src = '$foto[$i]'></td>
-                                <tr>
-
-
-                                   
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
-                        
-                            ";
-                        }
-                        
-                    }
-
-
-                ?>
-                </table>
-            </td>
         
+        <tr>
+        <?php
+        if($_SESSION['usuario_sessao'] != ''){
+            echo "<td width='30%'>
+            <table align='center' border='0' width =100%>
+                <tr  align='center'>   
+                    <td colspan='10' ><h1>Usuário</h1></td>
+                </tr>";
+                
+        
+                for($i=0;$i<sizeof($linhas_usuario);$i++){
+                    if($usuario[$i]==$_SESSION['usuario_sessao']){
+                        echo "
+                            <tr>   
+                                <th>Nome</th>    
+                                <td>$nome[$i]</td>
+                            <tr>
+                            <tr>   
+                                <th>Sobrenome</th>    
+                                <td>$sobrenome[$i]</td>
+                            <tr>
+                            <tr>   
+                                <th>Data de nascimento</th>    
+                                <td>$data[$i]</td>
+                            <tr>
+                            <tr>   
+                                <th>Enderço</th>    
+                                <td>$endereco[$i]</td>
+                            <tr>
+                            <tr>   
+                                <th>Telefone</th>    
+                                <td>$telefone[$i]</td>
+                            <tr>
+                            <tr>   
+                                <th>Sexo</th>    
+                                <td>$sexo[$i]</td>
+                            <tr>
+                            <tr>   
+                                <th>RG</th>    
+                                <td>$rg[$i]</td>
+                            <tr>
+                            <tr>   
+                                <th>CPF</th>    
+                                <td>$cpf[$i]</td>
+                            <tr>
+                            <tr>   
+                                <th>Login</th>    
+                                <td>$usuario[$i]</td>
+                            <tr>
+                            <tr>   
+                                <td colspan='2'><img width ='80%' src = '$foto[$i]'></td>
+                            <tr>
+                            ";
+                    }
+                }
+            echo "
+            </table>
+        </td>
+        ";
+        }
+        ?>
         <td width=70% valign='top'>
         <table align='center' border='0' width =100%>
                 <tr  align='center'>   
                     <td colspan='8' ><h1>Viagens oferecidas</h1></td>
                 </tr>
                 <tr align='center'>   
-                    <td>Destino</td>
-                    <td>Data de Partida</td>
-                    <td>Diárias</td>
-                    <td>Transporte</td>
-                    <td>Translado Pago</td>
-                    <td>Hospedagem</td>
-                    <td>Passeios Pago</td>
-                    <td>Custo</td>
+                    <th>Destino</th>
+                    <th>Data de Partida</th>
+                    <th>Diárias</th>
+                    <th>Transporte</th>
+                    <th>Translado Pago</th>
+                    <th>Hospedagem</th>
+                    <th>Passeios Pago</th>
+                    <th>Custo</th>
                 </tr>
 
                 <?php
                     for($i=0;$i<sizeof($linhas);$i++){
                         if($user[$i]=="" || $user[$i]==$_SESSION['usuario_sessao']){
+                            
                             $custo = $diarias[$i]*100 + $estrelas[$i]*500;
                             if($transportes[$i] == "Aviao"){
                                 $custo+=2000;
@@ -188,7 +179,7 @@
             
 
             </table>
-            </tr>
+        </tr>
         </table>
             
 
