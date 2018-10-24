eb187823f723143fc8f7ee855181a1fa87cd2af1
caiojvg <43935732+caiojvg@users.noreply.github.com>
2018-10-24 11:09:06 -0300

    Adicionado uso de banco de dados

--- login2.php
+++ login2.php
@@ -1,37 +1,47 @@
 <?php
 	session_start();
 	if(!isset($_SESSION['usuario_sessao'])){
-		$_SESSION["usuario_sessao"] = "";
+        $_SESSION["usuario_sessao"] = "";
+        $_SESSION["tipo_usuario"] = "";
+        $_SESSION["compra"] = "";
+    }
+    
+	//database information
+	$servername = "localhost";
+	$username = "root";
+	$password = "123456";
+	$dbname = "JoestarCompany";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+    
+    //user input
+	$usuario=$_POST['usuario'];
+    $senha=$_POST['senha'];
+    //check login
+    $login_valido = true;		
+    $sql = "select senha, status, tipo from Pessoa where login='".$usuario."';";
+	$sqlResultado = $conn->query($sql);
+	$consulta = mysqli_fetch_assoc($sqlResultado);
+	if($consulta["senha"]!=$senha || $consulta["status"]!='1'){
+		$login_valido=false;
 	}
-	
-		$usuario=$_POST['usuario'];
-		$senha=$_POST['senha'];
-        $login_valido = false;		
-        $arquivo_usuario = "arquivos/usuarios.txt";
-        $conteudo = file_get_contents($arquivo_usuario);
-        $linhas = explode ("\r\n", $conteudo);
+    
 
-        for ($i = 0; $i < sizeof($linhas); $i++) {
-            
-            $lista_usuarios [$i] = explode ("|", $linhas [$i]);
-        }
-
-        for ($i = 0; $i < sizeof($linhas); $i++) {
-            list($nome_s[$i], $sobrenome_s[$i], $data_s[$i], $endereco_s[$i], $telefone_s[$i], $sexo_s[$i], $rg_s[$i], $cpf_s[$i], $foto_s[$i], $usuario_s[$i], $senha_s[$i]) = explode ("|", $linhas [$i]);
-        }
+    //redirect to home    
+    if($login_valido){
+        $_SESSION["usuario_sessao"] = $usuario;
+        $_SESSION["tipo_usuario"] = $consulta["tipo"];
+        header("Location: home.php");
         
-        for($i=0; $i< sizeof($lista_usuarios); $i++){
-                if($usuario_s[$i] == $usuario && $senha_s[$i] == $senha){
-                    $login_valido = true;
-                }
-        }
-
-        if($login_valido){
-            header("Location: home.php");
-            $_SESSION["usuario_sessao"] = $usuario;
-            
-        } else{
-            header("Location: login.php"); /* Redirect browser */
-            exit();
-        }
+    //go back to login page        
+    } else{
+        header("Location: login.php"); /* Redirect browser */
+        exit();
+    }
 ?>
