cb7b3e2ab851a1c193d43b437e2cf3d7f51709bf
caiojvg <43935732+caiojvg@users.noreply.github.com>
2018-10-24 11:08:29 -0300

    Adicionado validações e exemplo de banco de dados

--- cadastro2.php
+++ cadastro2.php
@@ -2,63 +2,119 @@
 	session_start();
 	if(!isset($_SESSION['usuario_sessao'])){
 		$_SESSION["usuario_sessao"] = "";
-	}  
+    }  
+
+    //database information
+	$servername = "localhost";
+	$username = "root";
+	$password = "123456";
+	$dbname = "JoestarCompany";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+    
+    function validar_cpf($cpf){
+        //Verifica se o tamanho está correto
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+        //Verifica se o cpf é o mesmo número reptido 11 vezes
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+        //Cálculo de verificação do dígito identificador
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf{$c} * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf{$c} != $d) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+        //user input
         $rg=$_POST['rg'];
 		$cpf=$_POST['cpf'];
         $nome=$_POST['nome'];
         $sobrenome=$_POST['sobrenome'];
         $data_nascimento=$_POST['data_nascimento'];
-        $data_nascimento_date = date_create($data_nascimento);
-        $hoje_date = date_create(date('Y-m-d'));
 		$sexo=$_POST['sexo'];
 		$endereco=$_POST['endereco'];
 		$usuario=$_POST['usuario'];
 		$senha=$_POST['senha'];
         $telefone=$_POST['telefone'];
-        $cadastro_valido = true;		
-        $arquivo_usuario = "arquivos/usuarios.txt";
+        
+        //check inputs
+
+        $cadastro_valido = true;	
+        //check if CPF is already registered
+        $sql="select count(cpf) as quantidade from Pessoa where cpf = '".$cpf."';";
+		$sqlResultado = $conn->query($sql);
+		$consulta = mysqli_fetch_assoc($sqlResultado);
+        $qtdCpf = $consulta['quantidade'];
+
+        //check if RG is already registered
+        $sql="select count(cpf) as quantidade from Pessoa where cpf = '".$rg."';";
+		$sqlResultado = $conn->query($sql);
+		$consulta = mysqli_fetch_assoc($sqlResultado);
+		$qtdCpf = $consulta['quantidade'];
 		
-		if(strlen($nome)==0){
-            $cadastro_valido=false;
+		//check if login is already registered
+		$sql="select count(login) as quantidade from Pessoa where login = '".$login."';";
+		$sqlResultado = $conn->query($sql);
+		$consulta = mysqli_fetch_assoc($sqlResultado);
+		$qtdLogin = $consulta['quantidade'];
+		$today = date("Y-m-d");
+        
+        if(validar_cpf($cpf)==false){
+			$cadastroValido=false;
+		} else if($qtdCpf!=0){
+			$cadastroValido=false;
+		} else if(strlen($nome)==0){
+			$cadastroValido=false;
 		} else if(strlen($sobrenome)==0){
             $cadastro_valido=false;
-		} else if(strlen($usuario)==0){
-            $cadastro_valido=false;
-		} else if(strlen($senha)==0){
-            $cadastro_valido=false;
-		} else if($data_nascimento_date>$hoje_date){
-            $cadastro_valido=false;
-        } else if(filesize($arquivo_usuario) !=0){
-            $conteudo = file_get_contents($arquivo_usuario);
-            $linhas = explode ("\r\n", $conteudo);
-            for ($i = 0; $i < sizeof($linhas); $i++) {
-                list($nome_s[$i], $sobrenome_s[$i], $data_s[$i], $endereco_s[$i], $telefone_s[$i], $sexo_s[$i], $rg_s[$i], $cpf_s[$i], $foto_s[$i], $usuario_s[$i], $senha_s[$i]) = explode ("|", $linhas [$i]);
-            }
-
-            for($i=0; $i< sizeof($linhas); $i++){
-                if($usuario_s[$i] == $usuario){
-                    $cadastro_valido=false;
-                }
-            }
-        }
+		} else if(strlen($endereco)==0){
+			$cadastroValido=false;
+		} else if(strlen($login)==0){
+			$cadastroValido=false;
+		} else if($qtdLogin!=0){
+			$cadastroValido=false;
+		} else if(strlen($rg)==0){
+			$cadastroValido=false;
+		} else if($rg!=0){
+			$cadastroValido=false;
+		} else if(strlen($senha)<8){
+			$cadastroValido=false;
+		} else if(strlen($telefone)<8){
+			$cadastroValido=false;
+		} else if(strlen($data_nascimento)!=10){
+			$cadastroValido=false;
+		} else if(date_create($data_nascimento)>=date_create($today)){
+			$cadastroValido=false;
+		}
 		
         if($cadastro_valido){
             $caminho_foto="";
             if(filesize($_FILES['foto']!=0)){
-                $extensao = strtolower(substr($_FILES['foto']['name'],-4)); //Pegando //extensão do arquivo
+                $extensao = strtolower(substr($_FILES['foto']['name'],-4)); //Pegando extensão do arquivo
 
                 $diretorio = 'fotos/'; //Diretório para uploads
                 $caminho_foto = $diretorio . 'foto_' . $_POST['usuario']. $extensao;
-                move_uploaded_file($_FILES['foto']['tmp_name'], $caminho_foto); //Fazer upload do //arquivo
+                move_uploaded_file($_FILES['foto']['tmp_name'], $caminho_foto); //Fazer upload do arquivo
             }
-            $cadastro_texto = $nome . "|" . $sobrenome . "|" . $data_nascimento . "|" . $endereco . "|" . $telefone . "|" . $sexo . "|" . $rg . "|" .$cpf . "|" . $caminho_foto ."|" . $usuario . "|" . $senha;
-            $arquivo_usuario = "arquivos/usuarios.txt";
-            $conteudo = file_get_contents($arquivo_usuario);
-            if($conteudo!="")
-                $conteudo .= "\r\n".$cadastro_texto;
-            else
-                $conteudo .= $cadastro_texto;
-            file_put_contents($arquivo_usuario, $conteudo);
+            $sql="insert into Pessoa (cpf, rg, nome, sobrenome, sexo, endereco, telefone, dataNascimento, tipo, status, login,  senha, caminhoFoto) values ('".$cpf."', '".$rg."', '".$nome."', '".$sobrenome."', '".$sexo."', '".$endereco."', '".$telefone."', '".$data_nascimento."', '".$tipo."', '".$status."', '".$usuario."', '".$senha."', '".$caminho_foto."');"
+            $sqlResultado = $conn->query($sql);
+            
             header("Location: login.php");
         } else{
             header("Location: cadastro.php");
